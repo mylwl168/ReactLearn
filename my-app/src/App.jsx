634b1d7c29f3511541080abeca3c8d8cfb0617b6
mylwl168 <mylwl@hotmail.com>
2023-03-10 10:36:04 +0900

    Q3

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -54,6 +54,7 @@ export default function Game() {
   const [currentMove, setCurrentMove] = useState(0);
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
+  const [isAscending, setIsAscending] = useState(true); // Q3
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
@@ -93,12 +94,21 @@ export default function Game() {
     );
   });
 
+  if (!isAscending) { // Q3
+    moves.reverse();
+  }
+
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
+        <div>
+          <button onClick={() => setIsAscending(!isAscending)}>
+            {isAscending ? 'Descending' : 'Ascending'}
+          </button>
+        </div>
         <ol>{moves}</ol>
       </div>
     </div>
